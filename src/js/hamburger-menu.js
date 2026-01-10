eeea6f428824d0ff9f5a6b56dd67c4d8e53233d9
LentonNeale <lentonneale@gmail.com>
2026-01-10 04:32:01 +0000

    WIP: Save current changes before merging master

--- src/js/hamburger-menu.js
+++ src/js/hamburger-menu.js
@@ -1,5 +1,5 @@
 const hamburgerButton = document.querySelector('.hamburger');
 
 hamburgerButton.addEventListener('click', () => {
-    hamburgerButton.classList.toggle('hamburger--active'); // BEM modifier for active state
+    hamburgerButton.classList.toggle('hamburger--active');
 });
